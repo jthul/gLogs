bcbe228fc15536532179db4f9105590c4e5ff980
THULLIEZ <THULLIEZ>
2017-06-22 18:15:42 +0200

    Some comments

--- gLogs-war/src/main/java/org/gLogs/war/main/Main.java
+++ gLogs-war/src/main/java/org/gLogs/war/main/Main.java
@@ -6,16 +6,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-
 @ComponentScan("org.gLogs.war.status")
 @SpringBootApplication
-public class Main extends SpringBootServletInitializer{
-	
+public class Main extends SpringBootServletInitializer {
+
 	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
-	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Main.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
--- gLogs-war/src/main/java/org/gLogs/war/status/StatusController.java
+++ gLogs-war/src/main/java/org/gLogs/war/status/StatusController.java
@@ -5,12 +5,21 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 
+ * Status controller is a basic controller that will help 
+ * to know what are the server services status.
+ * 
+ * 
+ * @author jThulliez
+ *
+ * created 22 june 2017
+ */
 @Controller
 public class StatusController {
-	
-	@RequestMapping("/test")
+	@RequestMapping("/status")
     public String serverIsUp(Map<String, Object> model) {
-		model.put("message", "Toto is alive");
+		model.put("message", "Server is runing ! ");
 		return "serverIsUp";
     }
 	
