721eba4ad4aecbc7c1510350b46dd343ada50f26
Jérémy <Jrmy@DESKTOP-6MDCM8K>
2017-07-12 21:29:22 +0200

    Added the logo and the login component

--- gLogs-war/src/main/frontend/src/app/app.module.ts
+++ gLogs-war/src/main/frontend/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { ListLogsComponent } from './list-logs/list-logs.component';
 import { DisplayLogContentComponent } from './display-log-content/display-log-content.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     ListLogsComponent,
     DisplayLogContentComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [ListLogsComponent,HeaderComponent,DisplayLogContentComponent]
+  bootstrap: [ListLogsComponent,HeaderComponent,DisplayLogContentComponent,LoginComponent]
 })
 export class AppModule { }
